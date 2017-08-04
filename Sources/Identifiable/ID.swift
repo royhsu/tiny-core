58e213429868b7165dc3cddbdc1ca9306f4d6465
Roy Hsu <roy.hsu@tinyworld.cc>
2017-08-04 17:55:10 +0800

    add ID

---
+++ Sources/Identifiable/ID.swift
@@ -0,0 +1,39 @@
+// MARK: - ID
+
+protocol ID: Equatable, Hashable {
+
+    // MARK: Property
+
+    associatedtype RawValue: ExpressibleByUnicodeScalarLiteral, ExpressibleByStringLiteral, Comparable, Hashable, CustomStringConvertible
+
+    var rawValue: RawValue { get }
+
+}
+
+// MARK: - Equatable
+
+extension ID {
+
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+
+        return lhs.rawValue == rhs.rawValue
+
+    }
+
+}
+
+// MARK: - Hashable
+
+extension ID {
+
+    public var hashValue: Int { return rawValue.hashValue }
+
+}
+
+// MARK: - CustomStringConvertible
+
+extension ID {
+
+    public var description: String { return rawValue.description }
+
+}
