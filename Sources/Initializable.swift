aa8e1ff8dba59c8ca20e539eb172ed8ef1560e98
Roy Hsu <roy.hsu@tinyworld.cc>
2019-01-27 13:04:09 +0800

    add initializable protocol.

---
+++ Sources/Initializable.swift
@@ -0,0 +1,15 @@
+//
+//  Initializable.swift
+//  TinyCore
+//
+//  Created by Roy Hsu on 2019/1/27.
+//  Copyright © 2019 TinyWorld. All rights reserved.
+//
+
+// MARK: - Initializable
+
+public protocol Initializable {
+    
+    init()
+    
+}
