e4528b42473682f39b1a50facb69fb1259659a38
Roy Hsu <roy.hsu@tinyworld.cc>
2019-02-17 23:26:44 +0800

    add context.

---
+++ Sources/Context/Context.swift
@@ -0,0 +1,98 @@
+//
+//  Context.swift
+//  TinyCore
+//
+//  Created by Roy Hsu on 2019/2/17.
+//  Copyright © 2019 TinyWorld. All rights reserved.
+//
+
+// MARK: - Context
+
+public struct Context<Identifier> where Identifier: Hashable {
+    
+    private var storage: [Identifier: () throws -> Any] = [:]
+    
+    /// Register a factory for specific identifier.
+    public mutating func register(
+        _ factory: @escaping () throws -> Any,
+        for identifier: Identifier
+    ) { storage[identifier] = factory }
+    
+    /// Make a instance for specific identifier from the context.
+    /// If the identifier hasn't been registered, the context would throw an error.
+    /// Or specify the wrong target type, will also raise an error.
+    public func make<T>(
+        _ targetType: T.Type,
+        for identifier: Identifier
+    )
+    throws -> T {
+        
+        guard let factory = storage[identifier] else { throw Error.unregistered(identifier: identifier) }
+        
+        let instance = try factory()
+        
+        guard let typedInstance = instance as? T else {
+            
+            throw Error.typeMismatch(
+                identifier: identifier,
+                expectedType: targetType,
+                autualType: type(of: instance)
+            )
+            
+        }
+        
+        return typedInstance
+            
+    }
+    
+}
+
+extension Context {
+    
+    /// Register a factory for specific identifier.
+    public mutating func register(
+        _ factory: @autoclosure @escaping () throws -> Any,
+        for identifier: Identifier
+    ) {
+        
+        register(
+            { try factory() },
+            for: identifier
+        )
+        
+    }
+    
+    public mutating func register(
+        _ type: Initializable.Type,
+        for identifier: Identifier
+    ) {
+        
+        register(
+            type.init,
+            for: identifier
+        )
+        
+    }
+    
+}
+
+extension Context {
+    
+    public func make<T>(for identifier: Identifier) throws -> T {
+        
+        return try make(
+            T.self,
+            for: identifier
+        )
+        
+    }
+    
+}
+
+// MARK: - Error
+
+extension Context {
+    
+    private typealias Error = ContextError<Identifier>
+    
+}
---
+++ Tests/Context/ContextTests.swift
@@ -0,0 +1,157 @@
+//
+//  ContextTests.swift
+//  TinyCoreTests
+//
+//  Created by Roy Hsu on 2019/2/17.
+//  Copyright © 2019 TinyWorld. All rights reserved.
+//
+
+// MARK: - ContextTests
+
+import XCTest
+
+@testable import TinyCore
+
+final class ContextTests: XCTestCase {
+    
+    func testMakeInstancesWithRegisteredIdentifiers() {
+        
+        enum ProductProperties: Hashable { case price }
+        
+        var context = Context<ProductProperties>()
+        
+        context.register(
+            5.0,
+            for: .price
+        )
+        
+        XCTAssertEqual(
+            try context.make(
+                Double.self,
+                for: .price
+            ),
+            5.0
+        )
+        
+    }
+    
+    func testMakeInstancesWithUnregisteredIdentifiers() {
+        
+        enum ProductProperties: Hashable { case title }
+        
+        let context = Context<ProductProperties>()
+        
+        XCTAssertThrowsError(
+            try context.make(
+                String.self,
+                for: .title
+            )
+        ) { error in
+            
+            guard case ContextError<ProductProperties>.unregistered(identifier: .title) = error else {
+                
+                XCTFail("Unexpected error type. \(error)")
+                
+                return
+                
+            }
+            
+            XCTSuccess()
+
+        }
+        
+    }
+    
+    func testMakeInstancesWithRegisteredIdentifiersButSpecifiedWrongType() {
+        
+        enum ProductProperties: Hashable { case title }
+        
+        var context = Context<ProductProperties>()
+        
+        context.register(
+            "Chocolate",
+            for: .title
+        )
+        
+        XCTAssertThrowsError(
+            try context.make(
+                Int.self,
+                for: .title
+            )
+        ) { error in
+            
+            guard let contextError = error as? ContextError<ProductProperties> else {
+                
+                XCTFail("Unexpected error type. \(error)")
+                
+                return
+                
+            }
+            
+            switch contextError {
+                
+            case let .typeMismatch(identifier, expectedType, autualType):
+                
+                XCTAssertEqual(
+                    identifier,
+                    .title
+                )
+                
+                XCTAssert(expectedType is Int.Type)
+                
+                XCTAssert(autualType is String.Type)
+                
+            default: XCTFail("Unexpected error type. \(error)")
+                
+            }
+            
+        }
+        
+    }
+    
+    func testRegisterWithInitializale() {
+        
+        enum MessageProperties: Hashable { case user }
+        
+        struct User: Initializable, Equatable {
+            
+            let id = 1
+            
+        }
+        
+        var context = Context<MessageProperties>()
+        
+        context.register(
+            User.self,
+            for: .user
+        )
+        
+        XCTAssertEqual(
+            try context.make(
+                User.self,
+                for: .user
+            ),
+            User()
+        )
+        
+    }
+    
+    func testMakeInstancesWithTypeInference() {
+        
+        enum ProductProperties: Hashable { case title }
+        
+        var context = Context<ProductProperties>()
+        
+        context.register(
+            "Chocolate",
+            for: .title
+        )
+        
+        XCTAssertEqual(
+            try context.make(for: .title),
+            "Chocolate"
+        )
+        
+    }
+    
+}
